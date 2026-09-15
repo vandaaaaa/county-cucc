d9a1815bf23cbc5009c694e91e3662e91261b09c
vandaaaaa <acsvanda25@gmail.com>
2026-09-15 12:41:42 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\CountyController;
 
 Route::get('/', [App\Http\Controllers\CountyController::class, 'index'])->name('counties.index');
 Route::get('/counties', [App\Http\Controllers\CountyController::class, 'index'])->name('counties.index');
+Route::post('/counties', [CountyController::class, 'store'])->name('counties.store');
